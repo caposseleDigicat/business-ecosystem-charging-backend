b729b2802009aa60ec759378b091b81ba492a9de
danielpuschmann <daniel.puschmann@digicatapult.org.uk>
2019-10-24 13:55:03 +0100

    Allow duplicate urls (#18)
    * Allow duplicate urls for data source specifications
    
    * Allow duplicate urls for data source specifications
    
    * Removed code instead of uncomment

--- src/wstore/asset_manager/asset_manager.py
+++ src/wstore/asset_manager/asset_manager.py
@@ -187,8 +187,7 @@ class AssetManager:
         provided_as = 'FILE'
         if 'content' in data:
             if isinstance(data['content'], str) or isinstance(data['content'], unicode):
-                self._check_url_conflict(data, current_organization)
-
+                
                 download_link = data['content']
                 provided_as = 'URL'
 
